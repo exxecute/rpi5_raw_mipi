bcaf5ae61841c9160ca2ec846ef9ef68ec1fe602
Uladzislau Mikhayevich <uladzislau.m@noctavis.com>
2025-09-18 15:20:52 +0300

    chore: raw mipi
    add setup disk script

---
+++ setup_disk.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "Setting up image for RPI5 64-bit!"
+
+KERNEL=kernel_2712
+
+if [[ -v SD_CARD ]]; then
+    echo '[OK]: SD_CARD sourced "$SD_CARD"'
+else
+    echo '[ERROR]: Source SD_CARD (export SD_CARD="/dev/sdxxx")'
+fi
+
+# create mnt dir
+mkdir -p mnt
+mkdir -p mnt/boot
+mkdir -p mnt/root
+
+# Mount sd_card
+mount /dev/sdb1 mnt/boot
+mount /dev/sdb2 mnt/root
+
+# Install the kernel modules onto the boot media
+env PATH=$PATH make -j12 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=mnt/root modules_install
+
+# Backup image of the current kernel, install the fresh kernel image, overlays, README
+cp mnt/boot/$KERNEL.img mnt/boot/$KERNEL-backup.img
+cp arch/arm64/boot/Image mnt/boot/$KERNEL.img
+cp arch/arm64/boot/dts/broadcom/*.dtb mnt/boot/
+cp arch/arm64/boot/dts/overlays/*.dtb* mnt/boot/overlays/
+cp arch/arm64/boot/dts/overlays/README mnt/boot/overlays/
+
+# Umount sd_card
+umount mnt/boot
+umount mnt/root
